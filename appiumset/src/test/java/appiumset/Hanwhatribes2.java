334adf50c5a1dce4f8b570d83a6db19aa4599930
신준오 <@DESKTOP-F3B2TKP>
2022-10-10 00:10:59 +0900

    1. 안드로이드 스튜디오 연동 시도중

--- appiumset/src/test/java/appiumset/Hanwhatribes2.java
+++ appiumset/src/test/java/appiumset/Hanwhatribes2.java
@@ -28,6 +28,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidElement;
@@ -155,7 +156,10 @@ public class Hanwhatribes2 {
 			arrow.click();
 			Thread.sleep(1000);
 			System.out.println("온보딩 버튼 클릭 완료 ");
+			LogEntries logEntries = driver.manage().logs().get("driver");
+
 		
+			
 		}
 
 		
@@ -167,9 +171,7 @@ public class Hanwhatribes2 {
 			kakao.click();
 			System.out.println("카카오 클릭 완료");
 			 		                 
-			
-			
-			
+
 			driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout[2]/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.widget.Button")
 			.click();
 			System.out.println("카카오 계속하기 클릭 완료 ");
@@ -195,29 +197,29 @@ public class Hanwhatribes2 {
 			driver.findElementByXPath("(//android.view.View[@content-desc=\"블랙 투명 레이어\"])[4]")
 			.click();
 			System.out.println("뮤직 트라이브 진입");
-			Thread.sleep(3000);
-			
-			WebElement toastView = driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.widget.TextView"));
-			String text = toastView.getAttribute("입장후, 다양한 혜택 정보에 접근이 가능해요!");
-			System.out.println("토스트 팝업 확인"); 	
 			
 			
-			
-			WebDriverWait wait = new WebDriverWait(driver,20);
-			wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("arrow beside text in button")));
-			System.out.println("id 찾으면 넘어가보자"); 	
+			if (driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.widget.TextView")
+					.isDisplayed())
+			{System.out.println("토스트 팝업 존재");
+			MobileElement toastView = driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.widget.TextView");
+			String text = toastView.getText();
+			System.out.println(toastView.getText()); 	
+			}else{
+				System.out.println("토스트 팝업 없음 -- 이슈");  //@ xpath.isDisplayed로 하면 xpath 자체가 없는걸로 인식되어 fail 됨 = else 미출력 후 에러 상태 
+				driver.close();
+			}
 		
-			
 
-			if (driver.findElementsByAccessibilityId("out in TopBar").isEmpty())
-				{System.out.println("Out in Top 없는 상태 확인");
-					MobileElement tribesin1 = driver.findElementByAccessibilityId("arrow beside text in button");
-					tribesin1.click();
-					System.out.println("없는 상태라면 [입장하기] 버튼 입력"); 	
-				}else{
+			if (driver.findElementsByAccessibilityId("out in TopBar")
+					.isEmpty())
+			{System.out.println("Out in Top 없는 상태 확인");
+			MobileElement tribesin1 = driver.findElementByAccessibilityId("arrow beside text in button");
+			tribesin1.click();
+			System.out.println("없는 상태라면 [입장하기] 버튼 입력"); 	
+			}else{
 				System.out.println("Out in Top 존재 상태 -- 이슈"); 
 				driver.close();
-
 			}
 				
 			   
@@ -306,6 +308,18 @@ public class Hanwhatribes2 {
 			.click(); 
 			System.out.println("트라이브 홈 탭");
 			Thread.sleep(2000);
+			
+			
+			if (driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View[1]/android.view.View/android.widget.TextView")
+					.isDisplayed())
+			{System.out.println("가입한 트라이브 존재");
+			MobileElement toastView = driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View[1]/android.view.View/android.widget.TextView");
+			String text = toastView.getText();
+			System.out.println(toastView.getText()); 	
+			}else{
+				System.out.println("가입한 트라이브 없음 -- 이슈"); //@ xpath.isDisplayed로 하면 xpath 자체가 없는걸로 인식되어 fail 됨 = else 미출력 후 에러 상태 			
+			driver.close();
+			}
 			
 		
 			MobileElement notification = driver.findElementByAccessibilityId("notification in TopBar");
