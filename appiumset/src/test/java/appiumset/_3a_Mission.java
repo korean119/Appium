19e508733117e210057fa5397be40e35d9d341b5
dgham <dgham@DESKTOP-48R3L5L>
2022-11-07 00:17:48 +0900

    1. scroll 조정 * uiautomator

--- appiumset/src/test/java/appiumset/_3a_Mission.java
+++ appiumset/src/test/java/appiumset/_3a_Mission.java
@@ -41,42 +41,76 @@ public class _3a_Mission extends _0_Automation_Main {
 				 
 				
 	
-				MobileElement 뮤직트라이브 = driver.get().findElementByXPath("(//android.view.View[@content-desc=\"블랙 투명 레이어\"])[2]");
+				MobileElement 뮤직트라이브 = driver.get().findElementByXPath("(//android.view.View[@content-desc=\"블랙 투명 레이어\"])[1]");
 				뮤직트라이브.click();
-				System.out.println("골프 트라이브 진입");
+				System.out.println("러닝 트라이브 진입");
 				Thread.sleep(3000);
+			//------------------- 골프 진입
+			
+				String tkfkd = "러닝 트라이브에서 알려주는 Tips";
 				
-				// first scrollView
-				// FindElement
-				MobileElement element = (MobileElement) driver.get().findElement(MobileBy.AndroidUIAutomator(
-				        "new UiScrollable(new UiSelector().scrollable(true).instance(0))" +
-				         ".scrollIntoView(new UiSelector().text(\"미션 참여를 통해 다양한 혜택을 즐겨보세요!\"))"));
-
-				// second scrollView
-				// FindElement
-				MobileElement element2 = (MobileElement) driver.get().findElement(MobileBy.AndroidUIAutomator(
-				        "new UiScrollable(new UiSelector().scrollable(true).instance(1))" +
-				         ".scrollIntoView(new UiSelector().text(\"골프 트라이브에서 알려주는 Tips\"))"));
-
-
+				try {
+					MobileElement eel = driver.get().findElement(MobileBy.AndroidUIAutomator(
+							"new UiScrollable(new UiSelector().scrollable(true)).setAsVerticalList().flingBackward()" +
+							".scrollIntoView(new UiSelector().text(\"러닝 트라이브에서 알려주는 Tips\"))"));
+			
+					
+					System.out.println("AAA");
+					
+					
+				} catch (InvalidSelectorException e) {
+				   
+				}
+					
+			// 러닝 트라이브에서 알려주는 Tips
+			// 11월 러닝 트라이브만의 핫한 혜택
+			//	.flingToEnd()  이거 넣으면 0 = 중간, 1= 끝까지 감
+				System.out.println("AAA");
+					
+					
+				 /*MobileElement eel3 = (MobileElement) driver.get().findElement(MobileBy.AndroidUIAutomator(
+					        "new UiScrollable(new UiSelector().scrollable(true)).setMaxSearchSwipes(10)" +
+					         ".scrollIntoView(new UiSelector().text(\"러닝 트라이브에서 알려주는 Tips\"))"));
+				 System.out.println("C");
+			
+				MobileElement eel = driver.get().findElement(MobileBy.AndroidUIAutomator(
+						"new UiScrollable(new UiSelector().scrollable(true)).setAsVerticalList()" +
+						".scrollIntoView(new UiSelector().text (\"러닝 트라이브에서 알려주는 Tips\"))"));
+				 System.out.println("A");
+				 
+				 
+				 MobileElement eel2 = driver.get().findElement(MobileBy.AndroidUIAutomator(
+							"new UiScrollable(new UiSelector().scrollable(true)).setAsVerticalList()" +
+							".scrollIntoView(new UiSelector().text (\"러닝 트라이브에서 알려주는 Tips\"))"));
+				 System.out.println("B");
+		
+			
+			  String el = "러닝 트라이브에서 알려주는 Tips";
+			  MobileElement element = driver.get().findElement(MobileBy.AndroidUIAutomator(
+			  "new UiScrollable(new UiSelector().scrollable(true)" +
+			  ".scrollIntoView(new UiSelector().text(\"" + el + "\"))"));
+			  
+			  System.out.println("A");
+			  
+			  String e2 = "러닝 트라이브에서 알려주는 Tips";
+			  MobileElement element2 = driver.get().findElement(MobileBy.AndroidUIAutomator(
+			  "new UiScrollable(new UiSelector().scrollable(true).instance(1))" +
+			  ".scrollIntoView(new UiSelector().text(\"" + el + "\"))"));
+			  
+			  System.out.println("B");*/
+			 
+				   
 
 				//*[@id='benefit-faq']
-				driver.get().close();
 				
-				wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View/android.view.View[2]/android.view.View/android.widget.TextView")))
+				
+			/*	wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View/android.view.View[2]/android.view.View/android.widget.TextView")))
 				.click();    
 				System.out.println("P# 트라이브 홈 화면_[자세히 보기] 버튼 입력");
-				Thread.sleep(3000);
+				Thread.sleep(3000);*/
 				
 				
-				Set<String> contextNames = driver.get().getContextHandles();
-				for (String contextName : contextNames) {
-					System.out.println(contextName); }
-					
-			
-				driver.get().context((String) contextNames.toArray()[1]);
-				Thread.sleep(3000); // context 변경 할 시간 필요하기 떄문에 sleep 사용 
-				System.out.println("S# 트라이브 홈 화면_혜택 상세 화면 Context WebView로 변경");  
+	
 
 		
 				//*[@id='benefit-faq']
