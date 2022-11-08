fb0bc9b5ec305d75fa95e2f93ecc8ccebb70b0be
김다연 <@DESKTOP-3MTVBSA>
2022-11-08 17:53:45 +0900

    1. mission 스크립트 추가  * 스크롤 임시 보류 상태

--- appiumset/src/test/java/appiumset/_3a_Mission.java
+++ appiumset/src/test/java/appiumset/_3a_Mission.java
@@ -3,6 +3,7 @@ package appiumset;
 import static org.testng.Assert.assertEquals;
 
 import java.net.MalformedURLException;
+import java.util.HashMap;
 import java.util.Set;
 
 import org.openqa.selenium.By;
@@ -22,6 +23,9 @@ public class _3a_Mission extends _0_Automation_Main {
 	public static void Mission() throws MalformedURLException, InterruptedException {
 		
 		System.out.println("<<< Mission 진행 >>>");
+		
+		
+		
 		
 		WebDriverWait wait = new WebDriverWait(driver.get(), 10);
 		//----------------바텀시트
@@ -45,40 +49,12 @@ public class _3a_Mission extends _0_Automation_Main {
 	
 				MobileElement 뮤직트라이브 = driver.get().findElementByXPath("(//android.view.View[@content-desc=\"블랙 투명 레이어\"])[2]");
 				뮤직트라이브.click();
-				System.out.println("러닝 트라이브 진입");
+				System.out.println("골프 트라이브 진입");
 				Thread.sleep(3000);
 			//------------------- 골프 진입
 				
-				/*
-				try {
-				    driver.get().findElement(MobileBy.AndroidUIAutomator(
-				            "new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollToEnd(10)"));
-				} catch (InvalidSelectorException e) {
-				    
-				}
+			
 				
-				try {
-				    driver.get().findElement(MobileBy.AndroidUIAutomator(
-				            "new UiScrollable(new UiSelector().scrollable(true)).flingToEnd(10)"));
-				} catch (InvalidSelectorException e) {
-				    
-				}
-				
-				System.out.println("맨 끝");
-				Thread.sleep(3000);
-				
-				
-				MobileElement eelll = driver.get().findElement(MobileBy.AndroidUIAutomator(
-						  "new UiScrollable(new UiSelector().scrollable(true).instance(2)).setAsHorizontalList().flingForward()"));
-				
-				System.out.println("스와이프");
-				Thread.sleep(3000);
-				
-				MobileElement eellllll = driver.get().findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View[2]/android.view.View[2]/android.view.View");
-				eellllll.click();
-				System.out.println("클릭");
-				*/
-			//-------------------------------- 이거 트라이브 홈 스와이프	
 				new TouchAction(driver.get())
 				.longPress(PointOption.point(550, 1600))
 				.waitAction()
@@ -105,11 +81,111 @@ public class _3a_Mission extends _0_Automation_Main {
 				
 				//------------------혜택 상세
 				
+				Set<String> contextNames1 = driver.get().getContextHandles();
+				for (String contextName : contextNames1) {
+					System.out.println(contextName); }
+					
+			
+				driver.get().context((String) contextNames1.toArray()[1]);
+				Thread.sleep(3000); // context 변경 할 시간 필요하기 떄문에 sleep 사용
+				System.out.println("S# Context WebView로 변경");
+				Thread.sleep(3000);
 
-				String tkfkd = "포토갤러리";
+				MobileElement 시작하기버튼 = driver.get().findElementByXPath("//button[@id='mission-start-in-benefit']");
+				wait.until(ExpectedConditions.visibilityOf(시작하기버튼)).sendKeys(Keys.ENTER);
+				System.out.println("시작하기 버튼 입력");
+				Thread.sleep(3000);
+				
+				MobileElement 투표결과보기 = driver.get().findElementByXPath("//*[@id='poll-result-in-mission']");
 			
 				
-				 
+			
+				if(투표결과보기.isDisplayed()) {
+					
+					if(투표결과보기.isEnabled()) {
+						
+						System.out.println("버튼 활성화 이슈");
+						
+						driver.get().close();
+						
+					}else {
+						
+						System.out.println("버튼 비활성화 확인");
+						
+						Thread.sleep(2000);
+					}
+				
+					MobileElement 투표항목1 = driver.get().findElementByXPath("//label[@aria-label='투표항목1']");
+					wait.until(ExpectedConditions.visibilityOf(투표항목1)).click();
+					System.out.println("투표항목 1 입력"); 
+									
+					MobileElement 투표항목2 = driver.get().findElementByXPath("//label[@aria-label='투표항목2']");
+					wait.until(ExpectedConditions.visibilityOf(투표항목2)).click();
+					System.out.println("투표항목 2 입력"); 					
+
+					wait.until(ExpectedConditions.visibilityOf(투표항목1)).click();
+					System.out.println("투표항목 1 입력"); 			
+					
+					wait.until(ExpectedConditions.visibilityOf(투표항목2)).click();
+					System.out.println("투표항목 2 입력"); 
+									
+					if(투표결과보기.isEnabled()) {
+						
+						System.out.println("버튼 활성화 확인");
+						
+						Thread.sleep(2000);
+						
+					}else {
+						
+						System.out.println("버튼 비활성화 이슈");
+						
+						driver.get().close();
+						
+					}
+									
+					MobileElement 투표항목결과보기 = driver.get().findElementByXPath("//*[@id=\"poll-result-in-mission\"]");
+					wait.until(ExpectedConditions.visibilityOf(투표항목결과보기)).click();
+					System.out.println("투표항몽결과보기");
+					
+					Thread.sleep(2000);
+					
+					if(driver.get().findElementByXPath("//*[@id=\"notification\"]/section/div[2]/div\r\n").isDisplayed()) {
+
+					System.out.println("미션 완료 팝업 출력 확인");
+					
+					Thread.sleep(3000);
+					
+					}else {
+						
+						System.out.println("미션 완료 팝업 미출력 이슈");
+						
+						driver.get().close();				
+												
+					}
+					
+					MobileElement 확인버튼 = driver.get().findElementByXPath("//*[@id=\"notification\"]/section/div[2]/div/div[2]/button");
+					wait.until(ExpectedConditions.visibilityOf(확인버튼)).sendKeys(Keys.ENTER);
+					System.out.println("미션 완료 팝업 [확인] 버튼 입력");
+					Thread.sleep(2000);
+					
+					MobileElement xButton = driver.get().findElementByXPath("//*[@id=\"portal\"]/section/div/div/div[1]/div/button");
+					wait.until(ExpectedConditions.visibilityOf(xButton)).sendKeys(Keys.ENTER);
+					System.out.println("미션 완료 팝업 [확인] 버튼 입력");
+					Thread.sleep(2000);
+
+				//-------------------------------------------&&&& 여기까지 했음
+				}else if(driver.get().findElementByXPath("//*[@id='invite-in-mission']").isDisplayed()){
+					
+					
+				}else if(driver.get().findElementByXPath("//*[@id='image-upload-in-mission']").isDisplayed()) {
+					
+					
+				}
+			}
+			
+				
+				/*
+				String tkfkd = "포토갤러리";
 		
 				MobileElement eel = driver.get().findElement(MobileBy.AndroidUIAutomator(
 				  "new UiScrollable(new UiSelector().scrollable(true)).setAsVerticalList()"
@@ -140,13 +216,12 @@ public class _3a_Mission extends _0_Automation_Main {
 					System.out.println("False");
 				}
 				
+				*/
+				
 				//assertEquals("러닝 트라이브에서 알려주는 Tips",ererText);
 				
 				
-				
-				System.out.println("CCCCCCCCCCCCcc");
-				Thread.sleep(1000);
-				
+			
 				//이거 팁스
 				//MobileElement erer = driver.get().findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.compose.ui.platform.ComposeView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.widget.TextView[2]");
 
@@ -246,5 +321,5 @@ public class _3a_Mission extends _0_Automation_Main {
 
 
 		
-}
+
 
